f89e8031f70a860a45290c97ec0222188eb6d56c
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2025-09-05 17:32:46 +0600

    spring boot server created

---
+++ src/main/java/com/chain_reaction_server/chain_reaction_server/ChainReactionServerApplication.java
@@ -0,0 +1,13 @@
+package com.chain_reaction_server.chain_reaction_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainReactionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainReactionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chain_reaction_server/chain_reaction_server/ChainReactionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chain_reaction_server.chain_reaction_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainReactionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
